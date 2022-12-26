4adfb0907c9dae89c78cef4276f3d5aff56f05b2
timjini <hatim.jini@gmail.com>
2022-12-27 00:24:27 +0300

    tedaviler

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,7 +25,7 @@ export default function Hero() {
   
         <div className="mt-8 flex flex-wrap justify-center gap-4">
           <Link
-            className="block w-full rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring active:bg-indigo-500 sm:w-auto"
+            className="block  rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring active:bg-indigo-500 sm:w-auto"
             href="/iletisim"
           >
             HEMEN BİLGİ AL
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Footer from '../components/Footer'
+import 'dotenv/config'
+
+
 
 export default function Document() {
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
