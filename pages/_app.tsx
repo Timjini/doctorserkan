c187fe3e7df656247ab8fb3102ccb1ed67dd8ef3
timjini <hatim.jini@gmail.com>
2022-12-27 16:11:07 +0300

    google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (  
+  <>
+  <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=GTM-5RFKKC7" />
+  <Script id="google-analytics" 
+    strategy='afterInteractive'>
+    {`
+     window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'GTM-5RFKKC7');
+    `}
+    </Script>
+      <Component {...pageProps} />
+
+  </>
+  
+  )
+
+
 }
